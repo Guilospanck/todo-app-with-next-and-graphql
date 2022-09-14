98cfb01958e345974d8ad1c855fa90c63e55b7f1
Guilherme Pereira <guilherme.rp2010@hotmail.com>
2022-09-14 09:58:33 +0100

    Add `Note` graphql type

---
+++ src/graphql/Note.ts
@@ -0,0 +1,23 @@
+import { extendType, objectType } from 'nexus'
+
+export const Note = objectType({
+  name: 'Note',
+  definition(t) {
+    t.nonNull.int('id')
+    t.nonNull.string('title')
+    t.string('description')
+    t.nonNull.dateTime('createdAt')
+  },
+})
+
+export const NoteQuery = extendType({
+  type: 'Query',
+  definition(t) {
+    t.list.field('getAllNotes', {
+      type: 'Note',
+      resolve(parent, ags, context) {
+        return []
+      }
+    })
+  },
+})
