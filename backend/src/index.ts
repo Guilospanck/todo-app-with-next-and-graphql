80be48d796e525d61b483f4e4cc1c8942574797f
Guilherme Pereira <guilherme.rp2010@hotmail.com>
2022-09-15 10:40:14 +0100

    Change from `apollo-server` to `apollo-server-express`

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,25 +1,31 @@
-import { ApolloServer } from "apollo-server"
+import { ApolloServer } from "apollo-server-express"
 import StartPrisma, { prisma } from "./infrastructure/database/prisma"
 import { context } from "./infrastructure/middlewares/prisma/context_middleware"
 import { schema } from "./schema"
+import express from 'express'
+import http from 'http'
 
 StartPrisma()
   .catch(err => {
-    console.error(`Error tryng to start Prisma: ${err}`)
+    console.error(`Error trying to start Prisma: ${err}`)
     throw err
   })
   .finally(async () => {
     await prisma.$disconnect()
     console.log(`Prisma disconnected!`)
+  });
+
+(async function startApolloServer() {
+  const app = express()
+  const httpServer = http.createServer(app)
+  const server = new ApolloServer({
+    schema,
+    context,
+    csrfPrevention: true,
+    cache: 'bounded'
   })
-
-export const server = new ApolloServer({
-  schema,
-  context
-})
-
-const PORT = process.env.PORT || 4000
-
-server.listen({ port: PORT }).then(({ url }) => {
-  console.log(`🚀 Server ready at ${url}`)
-})
+  await server.start()
+  server.applyMiddleware({ app, path: '/api/graphql' })
+  await new Promise<void>(resolve => httpServer.listen({ port: 4000 }, resolve))
+  console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`)
+})()
