e77366985846ea6c784efba36c3f6c1550c08060
Guilherme Pereira <guilherme.rp2010@hotmail.com>
2022-09-16 17:15:24 +0100

    Add some styles to table

--- frontend/lib/home/views/index.tsx
+++ frontend/lib/home/views/index.tsx
@@ -1,25 +1,41 @@
-import { Fragment } from "react"
 import { IUseHomeViewModel } from "../viewModels/homeViewModel"
+import styles from './index.module.css'
 
 type HomeViewProps = {
   viewModel: IUseHomeViewModel
 }
 
 const HomeView = ({ viewModel }: HomeViewProps) => {
-  return <>
-    {viewModel.todoItems.length > 0 && viewModel.todoItems.map(item => {
-      return (
-        <Fragment key={item.id}>
-          <p>{item.id}</p>
-          <h1 className="text-3xl font-bold underline">{item.title}</h1>
-          <p>{item.description}</p>
-          <p>{item.createdAt}</p>
-          <p>{item.updatedAt}</p>
-          <p>{item.deletedAt}</p>
-        </Fragment>
-      )
-    })}
-  </>
+  return (
+    <table key={'table'} className="table-auto w-full">
+      <thead className="table-header-group">
+        <tr className="table-row-group">
+          <th className="table-cell">ID</th>
+          <th className="table-cell">Title</th>
+          <th className="table-cell">Description</th>
+          <th className="table-cell">Created at</th>
+          <th className="table-cell">Updated at</th>
+          <th className="table-cell">Deleted at</th>
+        </tr>
+      </thead>
+      <tbody>
+        {
+          viewModel.todoItems.length > 0 && viewModel.todoItems.map(item => {
+            return (
+              <tr>
+                <td>{item.id}</td>
+                <td className={styles.default_text}>{item.title}</td>
+                <td>{item.description}</td>
+                <td>{item.createdAt}</td>
+                <td>{item.updatedAt}</td>
+                <td>{item.deletedAt}</td>
+              </tr>
+            )
+          })
+        }
+      </tbody>
+    </table>
+  )
 }
 
 export default HomeView
