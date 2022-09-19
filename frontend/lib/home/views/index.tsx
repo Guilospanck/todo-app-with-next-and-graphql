3064c9444896062b8f6e78a26bf69abe108c970c
Guilherme Pereira <guilherme.rp2010@hotmail.com>
2022-09-19 11:30:49 +0100

    Add tailwind styles to table

--- frontend/lib/home/views/index.tsx
+++ frontend/lib/home/views/index.tsx
@@ -7,34 +7,36 @@ type HomeViewProps = {
 
 const HomeView = ({ viewModel }: HomeViewProps) => {
   return (
-    <table key={'table'} className="table-auto w-full">
-      <thead className="table-header-group">
-        <tr className="table-row-group">
-          <th className="table-cell">ID</th>
-          <th className="table-cell">Title</th>
-          <th className="table-cell">Description</th>
-          <th className="table-cell">Created at</th>
-          <th className="table-cell">Updated at</th>
-          <th className="table-cell">Deleted at</th>
-        </tr>
-      </thead>
-      <tbody>
-        {
-          viewModel.todoItems.length > 0 && viewModel.todoItems.map(item => {
-            return (
-              <tr>
-                <td>{item.id}</td>
-                <td className={styles.default_text}>{item.title}</td>
-                <td>{item.description}</td>
-                <td>{item.createdAt}</td>
-                <td>{item.updatedAt}</td>
-                <td>{item.deletedAt}</td>
-              </tr>
-            )
-          })
-        }
-      </tbody>
-    </table>
+    <div className="overflow-x-auto relative">
+      <table key={'table'} className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+        <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+          <tr>
+            <th scope="col" className="py-3 px-6">ID</th>
+            <th scope="col" className="py-3 px-6">Title</th>
+            <th scope="col" className="py-3 px-6">Description</th>
+            <th scope="col" className="py-3 px-6">Created at</th>
+            <th scope="col" className="py-3 px-6">Updated at</th>
+            <th scope="col" className="py-3 px-6">Deleted at</th>
+          </tr>
+        </thead>
+        <tbody>
+          {
+            viewModel.todoItems.length > 0 && viewModel.todoItems.map(item => {
+              return (
+                <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                  <td className="py-3 px-6">{item.id}</td>
+                  <td className={`${styles.default_text} py-3 px-6`}>{item.title}</td>
+                  <td className="py-3 px-6">{item.description}</td>
+                  <td className="py-3 px-6">{item.createdAt}</td>
+                  <td className="py-3 px-6">{item.updatedAt}</td>
+                  <td className="py-3 px-6">{item.deletedAt}</td>
+                </tr>
+              )
+            })
+          }
+        </tbody>
+      </table>
+    </div>
   )
 }
 
