73933ff1d886e5ce2aa02658b08352bb11191cac
Guilherme Pereira <guilherme.rp2010@hotmail.com>
2022-09-19 11:39:49 +0100

    Remanage tailwindcss class into css file

--- frontend/lib/home/views/index.tsx
+++ frontend/lib/home/views/index.tsx
@@ -11,12 +11,12 @@ const HomeView = ({ viewModel }: HomeViewProps) => {
       <table key={'table'} className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
         <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
           <tr>
-            <th scope="col" className="py-3 px-6">ID</th>
-            <th scope="col" className="py-3 px-6">Title</th>
-            <th scope="col" className="py-3 px-6">Description</th>
-            <th scope="col" className="py-3 px-6">Created at</th>
-            <th scope="col" className="py-3 px-6">Updated at</th>
-            <th scope="col" className="py-3 px-6">Deleted at</th>
+            <th scope="col" className={styles.default_td_th}>ID</th>
+            <th scope="col" className={styles.default_td_th}>Title</th>
+            <th scope="col" className={styles.default_td_th}>Description</th>
+            <th scope="col" className={styles.default_td_th}>Created at</th>
+            <th scope="col" className={styles.default_td_th}>Updated at</th>
+            <th scope="col" className={styles.default_td_th}>Deleted at</th>
           </tr>
         </thead>
         <tbody>
@@ -24,12 +24,12 @@ const HomeView = ({ viewModel }: HomeViewProps) => {
             viewModel.todoItems.length > 0 && viewModel.todoItems.map(item => {
               return (
                 <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-                  <td className="py-3 px-6">{item.id}</td>
-                  <td className={`${styles.default_text} py-3 px-6`}>{item.title}</td>
-                  <td className="py-3 px-6">{item.description}</td>
-                  <td className="py-3 px-6">{item.createdAt}</td>
-                  <td className="py-3 px-6">{item.updatedAt}</td>
-                  <td className="py-3 px-6">{item.deletedAt}</td>
+                  <td className={styles.default_td_th}>{item.id}</td>
+                  <td className={`${styles.default_text} ${styles.default_td_th}`}>{item.title}</td>
+                  <td className={styles.default_td_th}>{item.description}</td>
+                  <td className={styles.default_td_th}>{item.createdAt}</td>
+                  <td className={styles.default_td_th}>{item.updatedAt}</td>
+                  <td className={styles.default_td_th}>{item.deletedAt}</td>
                 </tr>
               )
             })
