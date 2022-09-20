55b133dbe38614dd94279b4d733637cc6e0b7add
Guilherme Pereira <guilherme.rp2010@hotmail.com>
2022-09-20 09:58:18 +0100

    Add custom `add todo item` modal

--- frontend/lib/home/views/index.tsx
+++ frontend/lib/home/views/index.tsx
@@ -9,12 +9,38 @@ type HomeViewProps = {
 }
 
 const HomeView = ({ viewModel }: HomeViewProps) => {
+
+  const modalFactory = () => {
+    const onCancelClick = viewModel.onModalCancelClick
+    const onConfirmClick = viewModel.onModalConfirmClick
+    const title = 'Add new Todo item'
+    const confirmTitle = 'Add'
+    const cancelTitle = 'Cancel'
+
+    const body =
+      <form className="flex flex-col gap-2">
+        <label className="text-sm">Title*</label>
+        <input id="title" type={'text'} className="appearance-none border rounded py-1 px-3 leading-tight focus:outline-none focus:shadow-outline" />
+        <label className="text-sm">Description</label>
+        <input id="description" type={'text'} className="appearance-none border rounded py-1 px-3 leading-tight focus:outline-none focus:shadow-outline" />
+      </form>
+
+    return <Modal
+      onCancelClick={onCancelClick}
+      onConfirmClick={onConfirmClick}
+      title={title}
+      confirmTitle={confirmTitle}
+      cancelTitle={cancelTitle}
+      body={body}
+    />
+  }
+
   return (
     <>
       {
         viewModel.showModal &&
         <div className="absolute w-screen h-screen">
-          <Modal onCancelClick={viewModel.onModalCancelClick} onConfirmClick={viewModel.onModalConfirmClick} />
+          {modalFactory()}
         </div>
       }
 
